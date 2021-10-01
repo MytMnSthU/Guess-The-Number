9e5e28072432df2d8f9044b7baa25016c1f929ab
Myat <myatminsithu0072@gmail.com>
2021-10-01 09:04:27 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,93 @@
+window.addEventListener("load", () => {
+	const randomNumber = Math.floor(Math.random() * 100),
+		submitNum = document.querySelector(".btn-guess");
+	var counter = 0,
+		inputTxt = document.querySelector(".input-text");
+	inputTxt.value = "";
+	inputTxt.focus();
+
+	var guessNumber = function () {
+		var inputNum = document.querySelector(".input-text").value,
+			answer = document.querySelector(".answer");
+
+		counter++;
+
+		if (inputNum > 10 && inputNum < 100) {
+			if (inputNum > randomNumber) {
+				answer.textContent = "Your answer is larger than than the number";
+				answer.style.color = "red";
+				answer.animate(
+					[
+						{
+							transform: "translateX(-5px)",
+						},
+						{
+							transform: " translateX(5px)",
+						},
+						{
+							transform: "translateX(-5px)",
+						},
+					],
+					{
+						duration: 200,
+						easing: "ease",
+						iterations: 1,
+					}
+				);
+			} else if (inputNum < randomNumber) {
+				answer.textContent = "Your answer is smaller than the number";
+				answer.style.color = "red";
+				answer.animate(
+					[
+						{
+							transform: "translateX(-5px)",
+						},
+						{
+							transform: " translateX(5px)",
+						},
+						{
+							transform: "translateX(-5px)",
+						},
+					],
+					{
+						duration: 200,
+						easing: "ease",
+						iterations: 1,
+					}
+				);
+			} else {
+				answer.textContent = "Your found the answer within " + counter + " times";
+				answer.style.color = "green";
+				answer.animate(
+					[
+						{
+							transform: "scale(0.98)",
+						},
+						{
+							transform: " scale(1.1)",
+						},
+						{
+							transform: "scale(0.98)",
+						},
+					],
+					{
+						duration: 1000,
+						easing: "ease-in",
+						iterations: 1,
+					}
+				);
+			}
+		} else {
+			answer.textContent = "You should enter a number between 10 and 100";
+			answer.style.color = "orange";
+		}
+	};
+
+	submitNum.addEventListener("click", guessNumber);
+	window.addEventListener("keydown", (e) => {
+		if (e.key == "Enter") {
+			guessNumber();
+		}
+	});
+	console.log(randomNumber);
+});
